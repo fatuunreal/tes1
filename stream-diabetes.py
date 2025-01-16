6f368a58b04c95bf1298ab7827b189d28f8336fc
fatuunreal <faturach03@gmail.com>
2025-01-16 19:08:50 +0700

    Update stream-diabetes.py

--- stream-diabetes.py
+++ stream-diabetes.py
@@ -1,48 +1,52 @@
 import pickle
 import streamlit as st
 
-# membaca model
-diabetes_model = pickle.load(open('diabetes_model.sav', 'rb'))
+# Membaca model
+diabetes_model = pickle.load(open('knn_diabetes_model.sav', 'rb'))
 
-#judul web
-st.title('Data Mining Prediksi Diabetes')
+# Judul web
+st.title('Prediksi Diabetes Dengan KNN new')
 
-#membagi kolom
+# Membagi kolom
 col1, col2 = st.columns(2)
 
-with col1 :
-    Pregnancies = st.text_input ('input nilai Pregnancies')
+with col1:
+    Pregnancies = st.text_input('Input nilai Pregnancies')
 
-with col2 :
-    Glucose = st.text_input ('input nilai Glucose')
+with col2:
+    Glucose = st.text_input('Input nilai Glucose')
 
-with col1 :
-    BloodPressure = st.text_input ('input nilai Blood Pressure')
+with col1:
+    BloodPressure = st.text_input('Input nilai Blood Pressure')
 
-with col2 :
-    SkinThickness = st.text_input ('input nilai Skin Thickness')
+with col2:
+    SkinThickness = st.text_input('Input nilai Skin Thickness')
 
-with col1 :
-    Insulin = st.text_input ('input nilai Insulin')
+with col1:
+    Insulin = st.text_input('Input nilai Insulin')
 
-with col2 :
-    BMI = st.text_input ('input nilai BMI')
+with col2:
+    BMI = st.text_input('Input nilai BMI')
 
-with col1 :
-    DiabetesPedigreeFunction = st.text_input ('input nilai Diabetes Pedigree Function')
+with col1:
+    DiabetesPedigreeFunction = st.text_input('Input nilai Diabetes Pedigree Function')
 
-with col2 :
-    Age = st.text_input ('input nilai Age')
+with col2:
+    Age = st.text_input('Input nilai Age')
 
-# code untuk prediksi
+# Variable untuk hasil prediksi
 diab_diagnosis = ''
 
-# membuat tombol untuk prediksi
+# Membuat tombol untuk prediksi
 if st.button('Test Prediksi Diabetes'):
-    diab_prediction = diabetes_model.predict([[Pregnancies, Glucose, BloodPressure, SkinThickness, Insulin, BMI, DiabetesPedigreeFunction, Age]])
-
-    if(diab_prediction[0] == 1):
-        diab_diagnosis = 'Pasien terkena Diabetes'
-    else:
-        diab_diagnosis = 'Pasien tidak terkena Diabetes'
-st.success(diab_diagnosis)
+    try:
+        # Validasi input (tidak boleh kosong)
+        if not all([Pregnancies, Glucose, BloodPressure, SkinThickness, Insulin, BMI, DiabetesPedigreeFunction, Age]):
+            diab_diagnosis = 'Semua kolom harus diisi'
+        else:
+            # Konversi input menjadi float
+            diab_prediction = diabetes_model.predict([[
+                float(Pregnancies),
+                float(Glucose),
+                float(BloodPressure),
+                float(Skin
